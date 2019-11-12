0a24ca869aa5225d0222fecf52635a1087651425
cbrwizard <cbrwizard@gmail.com>
2019-11-12 17:17:13 +1300

    Sets id for a version bump step

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const core = require('@actions/core')
 async function run() {
   try {
     const release = core.getInput('release')
+    if (!release) {
+      core.setFailed('no release received')
+    }
     console.log(`Received ${release}`)
-
-    core.debug(`Debug received ${release}`)
   } catch (error) {
     core.setFailed(error.message)
   }
